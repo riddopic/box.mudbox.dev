f766fcea82a160d73c3217fc2a14016221e4b6d2
Stefano Harding <riddopic@gmail.com>
2015-05-05 18:24:28 -0700

    drop CentOS 7.0 for 7.1

--- packer/scripts/centos/mudbox-local-gem.sh
+++ packer/scripts/centos/mudbox-local-gem.sh
@@ -1,6 +1,17 @@
 #!/bin/bash -eux
 
 echo 'Use local cache mirror of rubygems.org for speedup...'
-/opt/chef/embedded/bin/gem sources -a http://gems.dev:9292
-/opt/chef/embedded/bin/gem sources -r https://rubygems.org/
+cat <<EOF > /root/.gemrc
+---
+:backtrace: false
+:bulk_threshold: 1000
+:sources:
+- http://gems.dev:9292
+:update_sources: true
+:verbose: true
+install: "--no-rdoc --no-ri"
+update: "--no-rdoc --no-ri"
+benchmark: false
+gem: "--no-ri --no-rdoc"
+EOF
 echo "set -o vi" >> /root/.bashrc
--- packer/scripts/centos/mudbox-local-yum.sh
+++ packer/scripts/centos/mudbox-local-yum.sh
@@ -2,5 +2,61 @@
 
 echo 'Use local cache mirror of Yum repos for speedup...'
 rm -f /etc/yum.repos.d/*
-curl http://yum.mudbox.dev/centos/base.repo -o /etc/yum.repos.d/base.repo
-curl http://yum.mudbox.dev/centos/epel.repo -o /etc/yum.repos.d/epel.repo
+
+cat <<EOF > /etc/yum.repos.d/centos.repo
+[base]
+name=CentOS-$releasever - Base
+baseurl=http://yum.mudbox.dev/centos/$releasever/os/$basearch
+enabled=1
+gpgcheck=1
+gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-$releasever
+sslverify=true
+
+[extras]
+name=CentOS-$releasever - Extras
+baseurl=http://yum.mudbox.dev/centos/$releasever/extras/$basearch
+enabled=1
+gpgcheck=1
+gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-$releasever
+sslverify=true
+
+[updates]
+name=CentOS-$releasever - Updates
+baseurl=http://yum.mudbox.dev/centos/$releasever/updates/$basearch
+enabled=1
+gpgcheck=1
+gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-$releasever
+sslverify=true
+EOF
+
+cat <<EOF > /etc/yum.repos.d/chef_stable.repo
+[chef_stable]
+name=chef_stable
+baseurl=https://packagecloud.io/chef/stable/el/6/$basearch
+repo_gpgcheck=1
+gpgcheck=0
+enabled=1
+gpgkey=https://packagecloud.io/gpg.key
+sslverify=1
+sslcacert=/etc/pki/tls/certs/ca-bundle.crt
+
+[chef_stable-source]
+name=chef_stable-source
+baseurl=https://packagecloud.io/chef/stable/el/6/SRPMS
+repo_gpgcheck=1
+gpgcheck=0
+enabled=1
+gpgkey=https://packagecloud.io/gpg.key
+sslverify=1
+sslcacert=/etc/pki/tls/certs/ca-bundle.crt
+EOF
+
+cat <<EOF > /etc/yum.repos.d/epel.repo
+[epel]
+name=Extra Packages for Enterprise Linux 6 - $basearch
+baseurl=http://yum.mudbox.dev/fedora-epel/$releasever/$basearch
+enabled=1
+gpgcheck=1
+gpgkey=http://yum.mudbox.dev/fedora-epel/RPM-GPG-KEY-EPEL-$releasever
+sslverify=true
+EOF
